e6116ed9616f904e6665f4de315eb12290ab5e47
Marcus Collier <marcus@mjcollier.id.au>
2015-11-25 15:48:29 +0800

    converted to similar syntax

--- Berksfile
+++ Berksfile
@@ -3,4 +3,3 @@ site :opscode
 metadata
 
 cookbook 'apache2', git: 'https://github.com/meerkats/apache2.git', branch: 'COOK-3900'
-
--- recipes/default.rb
+++ recipes/default.rb
@@ -68,10 +68,10 @@ template "#{node['drupal']['dir']}/sites/default/settings.php" do
   source "d7.settings.php.erb"
   mode "0644"
   variables(
-    'database'        => node['drupal']['db']['database'],
-    'user'            => node['drupal']['db']['user'],
-    'password'        => node['drupal']['db']['password'],
-    'host'            => node['drupal']['db']['host']
+    'database'        => node[:drupal][:db][:database],
+    'user'            => node[:drupal][:db][:user],
+    'password'        => node[:drupal][:db][:password],
+    'host'            => node[:drupal][:db][:host]
   )
 end
 
